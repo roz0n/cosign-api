d9452ba36abbb95c4cbb4350b5971e809dc8bd6e
Arnold Rozon <arnold@rozon.org>
2022-09-17 18:26:15 -0700

    Working OAuth

--- pages/api/oauth.ts
+++ pages/api/oauth.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import * as HelloSignSDK from "hellosign-sdk";
 import axios from "axios";
 
 const SECRETS = {
@@ -15,39 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(500).send({ error: "state_mismatch" });
   }
 
-  console.log("code", code);
-  console.log("state", state);
-
   try {
-    // const response = await axios({
-    //   url: "https://app.hellosign.com/oauth/token?grant_type=authorization_code",
-    //   method: "POST",
-    //   params: {
-    //     state: state,
-    //     code: code,
-    //     grant_type: "authorization_code",
-    //     client_id: SECRETS.clientId,
-    //     client_secret: SECRETS.clientSecret,
-    //   },
-    // });
-
-    // if (response.status === 200) {
-    //   res.status(200).json(response.data.body);
-    // } else {
-    //   throw new Error();
-    // }
-
-    // let response = await fetch("https://app.hellosign.com/oauth/token?grant_type=authorization_code", {
-    //   method: "POST",
-    //   body: JSON.stringify({
-    //     state: state,
-    //     code: code,
-    //     clientId: SECRETS.clientId,
-    //     clientSecret: SECRETS.clientSecret,
-    //     grant_type: "authorization_code",
-    //   }),
-    // });
-
     let response = await axios({
       method: "POST",
       url: "https://app.hellosign.com/oauth/token",
@@ -66,11 +33,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (response.status === 200) {
       res.status(200).json(response.data);
     } else {
-      console.log(response.data);
-      throw new Error("Non-200 status code received");
+      throw new Error("Bad response");
     }
   } catch (error) {
-    console.log(error);
-    res.status(500).send({ error: "Error fetching token" });
+    console.log("Failed to obtain token", error);
+    res.status(500).send(error);
   }
 }
