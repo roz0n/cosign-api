a81803562bd126fb5d9667342b600cf83279163a
Arnold Rozon <arnold@rozon.org>
2022-09-18 19:12:44 -0700

    Adding oauth redirect

--- pages/auth/success.tsx
+++ pages/auth/success.tsx
@@ -1,6 +1,17 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
 
 const AuthSuccess: NextPage = () => {
+  const handleRedirect = () => {
+    console.log("Called");
+    window.location.replace("cosign://auth");
+  };
+
+  useEffect(() => {
+    console.log("Called two");
+    handleRedirect();
+  }, []);
+
   return (
     <div>
       <p>Login success</p>
