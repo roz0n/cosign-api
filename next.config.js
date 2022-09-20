7d5edce82b6bdd10f564bed99f04f25bb87b97bc
Arnold Rozon <arnold@rozon.org>
2022-09-19 20:40:01 -0700

    Added AASA file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  headers: async () => {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
