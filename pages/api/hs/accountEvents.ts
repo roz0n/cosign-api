e4cd8aeeb9ed7cef23490b2455ed97f7901320b2
Arnold Rozon <arnold@rozon.org>
2022-09-29 22:49:39 -0700

    Added missing account callback route

---
+++ pages/api/hs/accountEvents.ts
@@ -0,0 +1,28 @@
+import { EventCallbackAccountRequestPayload } from "hellosign-sdk";
+import type { NextApiRequest, NextApiResponse } from "next";
+import pusherOptions from "../../../helpers/pusherOptions";
+
+const Pusher = require("pusher");
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method !== "POST") {
+      res.status(405).json({ error: true });
+    }
+
+    if (!req.body) {
+      res.status(400).json({ error: true });
+    }
+
+    const pusher = new Pusher(pusherOptions);
+    const response = await pusher.trigger("cosign", "app-event", req.body);
+
+    if (response.status === 200) {
+      res.status(200).send("Hello API Event Received");
+    } else {
+      throw new Error("Invalid Pusher response");
+    }
+  } catch (error) {
+    res.status(500).json({ error: true });
+  }
+}
